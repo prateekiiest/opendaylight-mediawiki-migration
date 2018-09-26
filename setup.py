a18b77eb79134820db9454824511f88aee12f56e
Prateek Chanda <prateekkol21@gmail.com>
2018-09-26 07:15:49 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(
+    name='odl-mediawiki-migration',
+
+    description='A script that extracts mediawiki content from a Mediawiki site'
+                'and converts them to reStructuredText',
+    author='Prateek Chanda',
+    author_email='prateekkol21@gmail.com',
+    version='1.0.1',
+
+    url='https://github.com/prateekiiest/opendaylight-mediawiki-migration',
+
+    license="GPL 3.0",
+    keywords=['text', 'rst', 'mediawiki'],
+    scripts=['extractor.py']
+)
